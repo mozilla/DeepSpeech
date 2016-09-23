8d6755d93d557659ee610f9ed7b5eb5fa164d505
Kelly Davis <kdavis@mozilla.com>
2016-09-23 12:19:43 +0200

    Fix of issue #23

---
+++ util/gpu/__init__.py
@@ -0,0 +1,5 @@
+from tensorflow.python.client import device_lib
+
+def get_available_gpus():
+    local_device_protos = device_lib.list_local_devices()
+    return [x.name for x in local_device_protos if x.device_type == 'GPU']
