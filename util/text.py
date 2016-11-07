ca98c5aab85e485f304f44403a76bcb7bf25e1b6
Reuben Morais <reuben.morais@gmail.com>
2016-11-07 12:09:54 -0200

    Expose text_to_char_array in util/text.py

--- util/text.py
+++ util/text.py
@@ -6,31 +6,28 @@ SPACE_TOKEN = '<space>'
 SPACE_INDEX = 0
 FIRST_INDEX = ord('a') - 1  # 0 is reserved to space
 
+def text_to_char_array(original):
+    # Create list of sentence's words w/spaces replaced by ''
+    result = original.replace(" '", "") # TODO: Deal with this properly
+    result = result.replace("'", "")    # TODO: Deal with this properly
+    result = result.replace(' ', '  ')
+    result = result.split(' ')
+
+    # Tokenize words into letters adding in SPACE_TOKEN where required
+    result = np.hstack([SPACE_TOKEN if xt == '' else list(xt) for xt in result])
+
+    # Map characters into indicies
+    result = np.asarray([SPACE_INDEX if xt == SPACE_TOKEN else ord(xt) - FIRST_INDEX for xt in result])
+
+    # Add result to results
+    return result
 
 def texts_to_sparse_tensor(originals):
     # Define list to hold results
-    results = []
-
-    # Process each original in originals
-    for original in originals:
-        # Create list of sentence's words w/spaces replaced by ''
-        result = original.replace(" '", "") # TODO: Deal with this properly
-        result = result.replace("'", "")    # TODO: Deal with this properly
-        result = result.replace(' ', '  ')
-        result = result.split(' ')
-
-        # Tokenize words into letters adding in SPACE_TOKEN where required
-        result = np.hstack([SPACE_TOKEN if xt == '' else list(xt) for xt in result])
-
-        # Map characters into indicies
-        result = np.asarray([SPACE_INDEX if xt == SPACE_TOKEN else ord(xt) - FIRST_INDEX for xt in result])
-
-        # Add result to results
-        results.append(result)
+    results = [text_to_char_array(original) for original in originals]
 
     # Creating sparse representation to feed the placeholder
     return sparse_tuple_from(results)
-
 
 def sparse_tuple_from(sequences, dtype=np.int32):
     """Create a sparse representention of x.
@@ -48,7 +45,7 @@ def sparse_tuple_from(sequences, dtype=np.int32):
 
     indices = np.asarray(indices, dtype=np.int64)
     values = np.asarray(values, dtype=dtype)
-    shape = np.asarray([len(sequences), np.asarray(indices).max(0)[1]+1], dtype=np.int64)
+    shape = np.asarray([len(sequences), indices.max(0)[1]+1], dtype=np.int64)
 
     return tf.SparseTensor(indices=indices, values=values, shape=shape)
 
