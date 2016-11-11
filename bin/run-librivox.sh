0da42fbf17b8e858af33682437c83c5e309c2a53
Kelly Davis <kdavis@mozilla.com>
2016-11-11 07:09:00 +0100

    Fixed #174

---
+++ bin/run-librivox.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -xe
+
+ds_dataset_path="./data/ldc93s1/"
+export ds_dataset_path
+
+ds_importer="librivox"
+export ds_importer
+
+ds_batch_size=32
+export ds_batch_size
+
+ds_training_iters=15
+export ds_training_iters
+
+ds_validation_step=15
+export ds_validation_step
+
+if [ ! -f DeepSpeech.ipynb ]; then
+    echo "Please make sure you run this from DeepSpeech's top level directory."
+    exit 1
+fi;
+
+jupyter-nbconvert --to script DeepSpeech.ipynb --stdout | python
