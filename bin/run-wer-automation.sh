1766ba808b23158550a3fb27a4e9fad9b1e0a99a
Kelly Davis <kdavis-mozilla@users.noreply.github.com>
2016-11-16 22:37:11 +0100

    Fixed #201

--- bin/run-wer-automation.sh
+++ bin/run-wer-automation.sh
@@ -8,14 +8,20 @@ export ds_dataset_path
 ds_importer="ted"
 export ds_importer
 
-ds_batch_size=32
+ds_batch_size=64
 export ds_batch_size
 
-ds_training_iters=15
+ds_training_iters=30
 export ds_training_iters
 
 ds_validation_step=15
 export ds_validation_step
+
+ds_learning_rate=0.01
+export ds_learning_rate
+
+ds_display_step=10
+export ds_display_step
 
 ds_export_dir="/data/exports/`git rev-parse --short HEAD`"
 export ds_export_dir
