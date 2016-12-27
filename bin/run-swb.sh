d8ff6fcc64db4e94132c14166a2b62ec9901c6f9
Andre Natal <anatal@gmail.com>
2016-12-26 17:38:11 -0800

    Adding script to run swb

---
+++ bin/run-swb.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -xe
+
+export ds_importer="LDC97S62"
+
+export ds_train_batch_size=48
+export ds_dev_batch_size=32
+export ds_test_batch_size=32
+
+export ds_epochs=150
+export ds_display_step=10
+export ds_checkpoint_step=1
+
+if [ ! -f DeepSpeech.ipynb ]; then
+    echo "Please make sure you run this from DeepSpeech's top level directory."
+    exit 1
+fi;
+
+jupyter-nbconvert --to script DeepSpeech.ipynb --stdout | python -u
