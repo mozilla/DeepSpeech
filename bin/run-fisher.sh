53f76b30f8e07eee8b80c6ad0e144558db1cdaf3
Reuben Morais <reuben.morais@gmail.com>
2016-12-28 12:44:24 -0200

    Add bin/run-fisher.sh script for training with Fisher

---
+++ bin/run-fisher.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -xe
+
+export ds_importer="fisher"
+export ds_dataset_path="/data/LDC/"
+
+export ds_train_batch_size=32
+export ds_dev_batch_size=32
+export ds_test_batch_size=32
+
+export ds_epochs=20
+export ds_display_step=1
+export ds_validation_step=5
+export ds_checkpoint_step=5
+
+if [ ! -f DeepSpeech.ipynb ]; then
+    echo "Please make sure you run this from DeepSpeech's top level directory."
+    exit 1
+fi;
+
+jupyter-nbconvert --to script DeepSpeech.ipynb --stdout | python -u
