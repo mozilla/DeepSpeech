dc13d4be0691fbeae9a3a43f6632a56a1022c77e
Reuben Morais <reuben.morais@gmail.com>
2017-02-02 21:23:30 -0200

    Remove readme2tex infrastructure

--- doc/gendocs.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -xe
-
-python -m readme2tex --svgdir doc/svgs/ --username mozilla --project DeepSpeech --output doc/DeepSpeech.md doc/DeepSpeech.raw
-python -m readme2tex --svgdir doc/svgs/ --username mozilla --project DeepSpeech --output doc/Geometry.md doc/Geometry.raw
-python -m readme2tex --svgdir doc/svgs/ --username mozilla --project DeepSpeech --output doc/ParallelOptimization.md doc/ParallelOptimization.raw
