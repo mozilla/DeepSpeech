07620c4779c4ce4015604247fea9252973863b66
Reuben Morais <reuben.morais@gmail.com>
2017-03-28 16:40:35 +0200

    Export graph should take sequence lengths as input

--- native_client/deepspeech.cc
+++ native_client/deepspeech.cc
@@ -171,7 +171,7 @@ DsInfer(DeepSpeechContext* aCtx, float** aMfcc, int aNFrames)
 
   std::vector<Tensor> outputs;
   Status status =
-    aCtx->session->Run({{ "input_node", input }},
+    aCtx->session->Run({{ "input_node", input }, { "input_lengths", {n_frames} }},
                        {"output_node"}, {}, &outputs);
   if (!status.ok()) {
     std::cerr << "Error running session: " << status.ToString() << "\n";
