270b8beda5d4357acc895d34c45b0461fc8e54ae
Baljeet Kumar <baljeetkuma@gmail.com>
2017-04-04 18:21:09 +0530

    updated the hyperparameters as per the issue 333

--- bin/run-librivox.sh
+++ bin/run-librivox.sh
@@ -5,12 +5,18 @@ if [ ! -f DeepSpeech.py ]; then
     exit 1
 fi;
 
+
+
 python -u DeepSpeech.py \
   --importer librivox \
-  --train_batch_size 32 \
-  --dev_batch_size 32 \
-  --test_batch_size 32 \
+  --train_batch_size 12 \
+  --dev_batch_size 12 \
+  --test_batch_size 12 \
   --learning_rate 0.0001 \
-  --epoch 50 \
+  --epoch 15 \
   --display_step 5 \
+  --validationstep 5 \
+  --dropout_rate 0.30 \
+  --default_stddev 0.046875
+  --checkpoint_step 1
   "$@"
