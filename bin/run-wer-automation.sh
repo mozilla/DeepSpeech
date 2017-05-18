56f68a30f73256810bbda646be02494b8a5fff43
Tilman Kamp <tilmankamp@posteo.de>
2017-05-18 05:49:51 -0700

    Fix #583; timeout parameter for coordinator connections; minor fixes

--- bin/run-wer-automation.sh
+++ bin/run-wer-automation.sh
@@ -7,14 +7,13 @@ python -u DeepSpeech.py \
   --train_batch_size 16 \
   --dev_batch_size 8 \
   --test_batch_size 8 \
-  --epoch=-1 \
+  --epoch=50 \
   --learning_rate 0.0001 \
   --display_step 10 \
   --validation_step 10 \
   --dropout_rate 0.30 \
   --default_stddev 0.046875 \
   --checkpoint_step 1 \
-  --log_level 0 \
   --checkpoint_dir "${COMPUTE_KEEP_DIR}" \
   --wer_log_pattern "GLOBAL LOG: logwer('${COMPUTE_ID}', '%s', '%s', %f)"\
   "$@"
