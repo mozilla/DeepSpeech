a818e9a36a42a15b7b22c67d080501269afcb0ec
Rock de Vocht <peter@peter.co.nz>
2017-06-20 09:41:36 +1200

    halved librivox batch sizes, oom

--- bin/run-librivox.sh
+++ bin/run-librivox.sh
@@ -28,9 +28,9 @@ python -u DeepSpeech.py \
   --train_files "$COMPUTE_DATA_DIR/librivox-train-clean-100.csv,$COMPUTE_DATA_DIR/librivox-train-clean-360.csv,$COMPUTE_DATA_DIR/librivox-train-other-500.csv" \
   --dev_files "$COMPUTE_DATA_DIR/librivox-dev-clean.csv,$COMPUTE_DATA_DIR/librivox-dev-other.csv" \
   --test_files "$COMPUTE_DATA_DIR/librivox-test-clean.csv,$COMPUTE_DATA_DIR/librivox-test-other.csv" \
-  --train_batch_size 12 \
-  --dev_batch_size 12 \
-  --test_batch_size 12 \
+  --train_batch_size 6 \
+  --dev_batch_size 6 \
+  --test_batch_size 6 \
   --learning_rate 0.0001 \
   --epoch 15 \
   --display_step 5 \
