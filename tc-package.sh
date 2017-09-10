b42c83c859e9bed541e0f28cb8ff6c582b20b90c
Reuben Morais <reuben.morais@gmail.com>
2017-09-10 09:03:36 -0300

    Package LICENSE and README.mozilla with native_client.tar.xz

--- tc-package.sh
+++ tc-package.sh
@@ -17,9 +17,17 @@ tar -C ${DS_ROOT_TASK}/DeepSpeech/tf/bazel-bin/native_client/ \
 	libctc_decoder_with_kenlm.so \
 	generate_trie
 
+tar -C ${DS_ROOT_TASK}/DeepSpeech/ds/ \
+	-uf ${TASKCLUSTER_ARTIFACTS}/native_client.tar \
+	LICENSE
+
 tar -C ${DS_ROOT_TASK}/DeepSpeech/ds/native_client/ \
 	-uf ${TASKCLUSTER_ARTIFACTS}/native_client.tar \
 	deepspeech
+
+tar -C ${DS_ROOT_TASK}/DeepSpeech/ds/native_client/kenlm/ \
+	-uf ${TASKCLUSTER_ARTIFACTS}/native_client.tar \
+	README.mozilla
 
 if [ -d ${DS_ROOT_TASK}/DeepSpeech/ds/wheels ]; then
   cp ${DS_ROOT_TASK}/DeepSpeech/ds/wheels/* ${TASKCLUSTER_ARTIFACTS}/
