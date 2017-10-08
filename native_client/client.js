ef544dabd8234eb0b16e786e341eadfa962c17e7
Reuben Morais <reuben.morais@gmail.com>
2017-10-08 12:10:05 -0300

    Factor out feature number and context window size constants

--- native_client/client.js
+++ native_client/client.js
@@ -8,13 +8,16 @@ const LM_WEIGHT = 2.15;
 const WORD_COUNT_WEIGHT = -0.10;
 const VALID_WORD_COUNT_WEIGHT = 1.10;
 
+const N_FEATURES = 26;
+const N_CONTEXT = 9;
+
 var audioStream = new MemoryStream();
 Fs.createReadStream(process.argv[3]).
   pipe(Sox({ output: { bits: 16, rate: 16000, channels: 1, type: 'raw' } })).
   pipe(audioStream);
 audioStream.on('finish', () => {
   audioBuffer = audioStream.toBuffer();
-  var model = new Ds.Model(process.argv[2], 26, 9, process.argv[4]);
+  var model = new Ds.Model(process.argv[2], N_FEATURES, N_CONTEXT, process.argv[4]);
 
   if (process.argv.length > 6) {
      model.enableDecoderWithLM(process.argv[4], process.argv[5], process.argv[6],
--- native_client/client.py
+++ native_client/client.py
@@ -11,10 +11,14 @@ LM_WEIGHT = 2.15
 WORD_COUNT_WEIGHT = -0.10
 VALID_WORD_COUNT_WEIGHT = 1.10
 
-ds = Model(sys.argv[1], 26, 9, sys.argv[3])
+N_FEATURES = 26
+N_CONTEXT = 9
+
+ds = Model(sys.argv[1], N_FEATURES, N_CONTEXT, sys.argv[3])
 
 if len(sys.argv) > 5:
     ds.enableDecoderWithLM(sys.argv[3], sys.argv[4], sys.argv[5], BEAM_WIDTH,
                            LM_WEIGHT, WORD_COUNT_WEIGHT, VALID_WORD_COUNT_WEIGHT)
+
 fs, audio = wav.read(sys.argv[2])
 print(ds.stt(audio, fs))
