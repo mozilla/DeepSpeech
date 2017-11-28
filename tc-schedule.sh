e5d31be423be2fb48ffc4ec1d75d3c6e9d5e9783
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2017-11-28 13:10:27 +0100

    Adding build-inhibition handling
    Fixes #1035
    
    X-DeepSpeech: NOBUILD

--- tc-schedule.sh
+++ tc-schedule.sh
@@ -6,8 +6,8 @@ curdir=$(dirname "$0")
 
 pip3 install --quiet --user --upgrade pip
 
-curl -L --silent https://raw.githubusercontent.com/lissyx/taskcluster-github-decision/0ee93e9f36ef1fc77218a96e865daa80d36b87e5/requirements.txt | pip3 install --quiet --user --upgrade -r /dev/stdin
-curl -L --silent https://raw.githubusercontent.com/lissyx/taskcluster-github-decision/0ee93e9f36ef1fc77218a96e865daa80d36b87e5/tc-decision.py > ${curdir}/tc-decision.py
+curl -L --silent https://raw.githubusercontent.com/lissyx/taskcluster-github-decision/${TC_DECISION_SHA}/requirements.txt | pip3 install --quiet --user --upgrade -r /dev/stdin
+curl -L --silent https://raw.githubusercontent.com/lissyx/taskcluster-github-decision/${TC_DECISION_SHA}/tc-decision.py > ${curdir}/tc-decision.py
 
 # First, perform dry run for push and pull request
 # This should help us track merge failures in advance
