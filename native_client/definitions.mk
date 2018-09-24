a09ded9e28fd8d9016ca2908c4995de7990c6f2c
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2018-09-24 14:46:46 +0200

    Hack

--- native_client/definitions.mk
+++ native_client/definitions.mk
@@ -40,7 +40,7 @@ endif # ($(TARGET),rpi3)
 ifeq ($(TARGET),rpi3-armv8)
 TOOLCHAIN   ?= ${TFDIR}/bazel-$(shell basename "${TFDIR}")/external/LinaroAarch64Gcc72/bin/aarch64-linux-gnu-
 RASPBIAN    ?= $(abspath $(NC_DIR)/../multistrap-raspbian64-stretch)
-CFLAGS      := -march=armv8-a -mtune=cortex-a53 -D_GLIBCXX_USE_CXX11_ABI=0 -isystem $(RASPBIAN)/usr/include -isystem $(RASPBIAN)/usr/include/aarch64-linux-gnu
+CFLAGS      := -march=armv8-a -mtune=cortex-a53 -D_GLIBCXX_USE_CXX11_ABI=0 -isystem $(RASPBIAN)/usr/include/aarch64-linux-gnu
 CXXFLAGS    := $(CFLAGS)
 LDFLAGS     := $(RASPBIAN)/lib/aarch64-linux-gnu/libc.so.6 -Wl,-rpath-link,$(RASPBIAN)/lib/aarch64-linux-gnu/ -Wl,-rpath-link,$(RASPBIAN)/usr/lib/aarch64-linux-gnu/
 
