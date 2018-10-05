24d913658b6da71911fcb94759cf9e34b098d65b
Tilman Kamp <5991088+tilmankamp@users.noreply.github.com>
2018-10-05 23:32:43 +0200

    WIP

--- bin/prepare_sets.sh
+++ bin/prepare_sets.sh
@@ -4,6 +4,7 @@ data="${DATA_ROOT}/shared/data"
 fis="${data}/LDC/fisher"
 swb="${data}/LDC/LDC97S62/swb"
 lbs="${data}/OpenSLR/LibriSpeech/librivox"
+cv="${data}/mozilla/CommonVoice/v2.0-alpha1.0/en/cv_en_valid_"
 
 target_dir="$1"
 
@@ -15,6 +16,7 @@ vocoto \
     add "${lbs}-train-other-500.csv" \
     add "${fis}-train.csv" \
     add "${swb}-train.csv" \
+    add "${cv}train.csv" \
     shuffle \
     set original \
     hdf5 data/alphabet.txt "${target_dir}/ds_clean_train.hdf5" \
@@ -70,6 +72,6 @@ vocoto \
     add result \
     hdf5 data/alphabet.txt "${target_dir}/ds_noise2_train.hdf5"
 
-vocoto add "${lbs}-dev-clean.csv" hdf5 data/alphabet.txt "${target_dir}/ds_dev.hdf5"
+vocoto add "${lbs}-dev-clean.csv" add "${cv}dev.csv" hdf5 data/alphabet.txt "${target_dir}/ds_dev.hdf5"
 
-vocoto add "${lbs}-test-clean.csv" hdf5 data/alphabet.txt "${target_dir}/ds_test.hdf5"
+vocoto add "${lbs}-test-clean.csv" add "${cv}test.csv" hdf5 data/alphabet.txt "${target_dir}/ds_test.hdf5"
