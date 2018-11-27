ae5c67a1de3f77925e903630121c097e6eacefe9
josh <jmeyer@mozilla.com>
2018-11-26 17:11:55 -0800

    merging from master

--- filter_train_dev_test.py
+++ filter_train_dev_test.py
@@ -4,8 +4,8 @@ import os
 import ntpath
 
 '''
-USAGE:  $ python3 filter_cv1_dev_test.py LOCALE SAVE_TO_DIR
- e.g.:  $ python3 filter_cv1_dev_test.py 'ky' ../keep
+USAGE:  $ python3 filter_cv1_dev_test.py DATA_DIR LOCALE CLIPS.TSV SAVE_TO_DIR
+ e.g.:  $ python3 filter_cv1_dev_test.py "~/CV" 'ky' ~/clips.tsv ../keep
 
 
 The following script takes two files, clips.tsv and cv_LOCALE_valid.csv, and
@@ -23,12 +23,11 @@ the cv_LOCALE_valid.csv file.
 
 ## Expected to exist:
 #
-# input_folder/
+# data_dir/
 #     LOCALE/
 #         valid/
 #             *.wav
 #         cv_LOCALE_valid.csv (the wav_filename actually includes "valid/")
-#     clips.tsv
 
 ## Created by script:
 #
@@ -40,9 +39,10 @@ the cv_LOCALE_valid.csv file.
 
 
 
-LOCALE = sys.argv[1]
-input_folder =  sys.argv[2]
-output_folder = sys.argv[3]
+data_dir =  sys.argv[1]
+LOCALE = sys.argv[2]
+clips_tsv =  sys.argv[3]
+output_folder = sys.argv[4]
 
 
 
@@ -55,16 +55,33 @@ output_folder = sys.argv[3]
 # whether or not they've been validated (the file is called clips.tsv) 
 # clips.tsv ==  path	sentence    up_votes	down_votes   age     gender	accent	locale	bucket
 
-print("Looking for clips.tsv here: ", '{}/clips.tsv'.format(input_folder))
-clips = pandas.read_csv('{}/clips.tsv'.format(input_folder), sep='\t')
+print("Looking for clips.tsv here: ", clips_tsv)
+clips = pandas.read_csv(clips_tsv, sep='\t')
 # pull out data for just one language
 locale = clips[clips['locale'] == LOCALE]
-# format file names
+
+
+### REASSIGN TEXT / DEV / TRAIN ###
+locale['ID'] = locale['path'].str.split('/', expand = True)[0]
+speaker_counts = locale['ID'].value_counts()
+speaker_counts = speaker_counts.to_frame()
+speaker_counts.to_csv("speaker_count.csv", sep="\t")
+
+speaker_counts['ID'] = pandas.Series(speaker_counts.index).values
+num_spks = len(speaker_counts.index)
+train = ['train']*8
+dev = ['dev']*1
+test = ['test']*1
+splits = train + dev + test
+speaker_counts['new_bucket'] = pandas.Series((splits*num_spks)[:num_spks]).values
+locale['new_bucket']=locale['ID'].map(speaker_counts.set_index('ID')['new_bucket'])
+### ONLY WORKS FOR LANGS WITH MORE and MORE EVEN DATA ###
+
 locale['path'] = locale['path'].str.replace('/', '___')
 locale['path'] = locale['path'].str.replace('mp3', 'wav')
-dev_paths = locale[locale['bucket'] == 'dev'].loc[:, ['path']]
-test_paths = locale[locale['bucket'] == 'test'].loc[:, ['path']]
-train_paths = locale[locale['bucket'] == 'train'].loc[:, ['path']]
+dev_paths = locale[locale['new_bucket'] == 'dev'].loc[:, ['path']]
+test_paths = locale[locale['new_bucket'] == 'test'].loc[:, ['path']]
+train_paths = locale[locale['new_bucket'] == 'train'].loc[:, ['path']]
 
 
 
@@ -74,9 +91,9 @@ train_paths = locale[locale['bucket'] == 'train'].loc[:, ['path']]
 
 # cv_LANG_valid.csv == wav_filename,wav_filesize,transcript
 
-validated_clips = pandas.read_csv('{}/{}/cv_{}_valid.csv'.format(input_folder, LOCALE, LOCALE))
+validated_clips = pandas.read_csv('{}/{}/cv_{}_valid.csv'.format(data_dir, LOCALE, LOCALE))
 validated_clips['path'] = validated_clips['wav_filename'].apply(ntpath.basename)
-validated_clips['transcript'] =  validated_clips['transcript'].str.replace(u'\xa0', ' ') # for ky only?
+validated_clips['transcript'] =  validated_clips['transcript'].str.replace(u'\xa0', ' ')
 
 
 
@@ -90,6 +107,14 @@ dev_indices = validated_clips['path'].isin(dev_paths['path'])
 test_indices = validated_clips['path'].isin(test_paths['path'])
 train_indices = validated_clips['path'].isin(train_paths['path'])
 validated_clips = validated_clips.drop(columns=['path'])
+validated_clips['wav_filename'] =  data_dir + "/" + LOCALE + "/" + validated_clips['wav_filename'].astype(str)
+
+
+
+
+####              ####
+#### SAVE TO DISK ####
+####              ####
 
 print("###############################################")
 print("FILTERED CLIPS FOR THE LANGUAGE: ", str(LOCALE))
@@ -97,9 +122,6 @@ print("Num validated clips to be used in DEV: ", validated_clips[dev_indices]['w
 print("Num validated clips to be used in TEST: ", validated_clips[test_indices]['wav_filename'].count())
 print("Num validated clips to be used in TRAIN: ", validated_clips[train_indices]['wav_filename'].count())
 print("###############################################")
-
-ABS_PATH_TO_DATA = input_folder + "/" + LOCALE + "/"
-validated_clips['wav_filename'] =  ABS_PATH_TO_DATA + validated_clips['wav_filename'].astype(str)
 
 validated_clips[dev_indices].to_csv(os.path.join(output_folder, 'valid_dev.csv'), index=False)
 validated_clips[test_indices].to_csv(os.path.join(output_folder, 'valid_test.csv'), index=False)
