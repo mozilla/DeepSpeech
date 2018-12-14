33f5b640e44986356662c74afb1abadc6ded62fe
Carlos Fonseca M <carlfm01@hotmail.com>
2018-12-14 14:44:46 -0600

    Add CSharp client + examples

---
+++ examples/net_framework/CSharpExamples/DeepSpeechClient/Interfaces/IDeepSpeech.cs
@@ -0,0 +1,32 @@
+﻿namespace DeepSpeechClient.Interfaces
+{
+    public interface IDeepSpeech
+    {
+        void PrintVersions();
+
+        unsafe int CreateModel(string aModelPath, uint aNCep,
+                   uint aNContext,
+                   string aAlphabetConfigPath,
+                   uint aBeamWidth);
+
+        unsafe int EnableDecoderWithLM(string aAlphabetConfigPath,
+                  string aLMPath,
+                  string aTriePath,
+                  float aLMWeight,
+                  float aValidWordCountWeight);
+
+        unsafe string SpeechToText(short[] aBuffer,
+                uint aBufferSize,
+                uint aSampleRate);
+
+        unsafe void DiscardStream();
+
+        unsafe int SetupStream(uint aPreAllocFrames, uint aSampleRate);
+
+        unsafe void FeedAudioContent(short[] aBuffer, uint aBufferSize);
+
+        unsafe string IntermediateDecode();
+
+        unsafe string FinishStream();
+    }
+}
