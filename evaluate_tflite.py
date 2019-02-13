e9ad9b39a1680491579525a188b9273ec6c47518
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2019-02-13 17:35:45 +0100

    Limit amount of processes

--- evaluate_tflite.py
+++ evaluate_tflite.py
@@ -7,6 +7,7 @@ import numpy as np
 import wave
 import csv
 import sys
+import os
 
 from six.moves import zip, range
 from multiprocessing import JoinableQueue, Pool, Process, Queue, cpu_count
@@ -34,7 +35,8 @@ LM_BETA = 1.85
 N_FEATURES = 26
 N_CONTEXT = 9
 
-def tflite_worker(model, alphabet, lm, trie, queue_in, queue_out):
+def tflite_worker(model, alphabet, lm, trie, queue_in, queue_out, gpu_mask):
+    os.environ['CUDA_VISIBLE_DEVICES'] = str(gpu_mask)
     ds = Model(model, N_FEATURES, N_CONTEXT, alphabet, BEAM_WIDTH)
     ds.enableDecoderWithLM(alphabet, lm, trie, LM_ALPHA, LM_BETA)
 
@@ -64,14 +66,16 @@ def main():
                         help='Path to the language model trie file created with native_client/generate_trie')
     parser.add_argument('--csv', required=True,
                         help='Path to the CSV source file')
+    parser.add_argument('--proc', required=False, default=cpu_count(), type=int,
+                        help='Number of processes to spawn, defaulting to number of CPUs')
     args = parser.parse_args()
 
     work_todo = JoinableQueue()   # this is where we are going to store input data
     work_done = Queue()  # this where we are gonna push them out
 
     processes = []
-    for i in range(cpu_count()):
-        worker_process = Process(target=tflite_worker, args=(args.model, args.alphabet, args.lm, args.trie, work_todo, work_done), daemon=True, name='tflite_process_{}'.format(i))
+    for i in range(args.proc):
+        worker_process = Process(target=tflite_worker, args=(args.model, args.alphabet, args.lm, args.trie, work_todo, work_done, i), daemon=True, name='tflite_process_{}'.format(i))
         worker_process.start()        # Launch reader() as a separate python process
         processes.append(worker_process)
 
