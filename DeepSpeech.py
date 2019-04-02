989cfd1a02611603664109777b0c42cb52ef9969
josh <jmeyer@mozilla.com>
2019-04-02 02:19:33 +0200

    new transfer-learning branch on top of new changes to master which remove the MonitoredTrainingSession

--- DeepSpeech.py
+++ DeepSpeech.py
@@ -221,7 +221,7 @@ def create_optimizer():
 # on which all operations within the tower execute.
 # For example, all operations of 'tower 0' could execute on the first GPU `tf.device('/gpu:0')`.
 
-def get_tower_results(model_feeder, optimizer, dropout_rates):
+def get_tower_results(model_feeder, optimizer, dropout_rates, drop_source_layers):
     r'''
     With this preliminary step out of the way, we can for each GPU introduce a
     tower for which's batch we calculate and return the optimization gradients
@@ -251,9 +251,25 @@ def get_tower_results(model_feeder, optimizer, dropout_rates):
                     # Retain tower's avg losses
                     tower_avg_losses.append(avg_loss)
 
-                    # Compute gradients for model parameters using tower's mini-batch
-                    gradients = optimizer.compute_gradients(avg_loss)
+                    # # Compute gradients for model parameters using tower's mini-batch
+                    # gradients = optimizer.compute_gradients(avg_loss)
 
+                    if FLAGS.fine_tune:
+                        # train from source model and fine-tine
+                        # aka - update all layers
+                        gradients = optimizer.compute_gradients(avg_loss)
+                    else:
+                        # train from source model and freeze old layers
+                        # aka - only update new layers
+                        gradients = optimizer.compute_gradients(
+                            avg_loss,
+                            var_list = [ v for v in tf.trainable_variables()
+                                         if any(
+                                                 layer in v.op.name
+                                                 for layer in drop_source_layers
+                                         )]
+                        )
+                    
                     # Retain tower's gradients
                     tower_gradients.append(gradients)
 
@@ -376,6 +392,19 @@ def train():
     If no server provided, it performs single process training.
     '''
 
+    # The transfer learning approach here need us to supply the layers which we
+    # want to exclude from the source model.
+    # Say we want to exclude all layers except for the first one, we can use this:
+    #
+    #    drop_source_layers=['2', '3', 'lstm', '5', '6']
+    #
+    # If we want to use all layers from the source model except the last one, we use this:
+    #
+    #    drop_source_layers=['6']
+    #
+
+    drop_source_layers = ['2', '3', 'lstm', '5', '6'][-int(FLAGS.drop_source_layers):]
+
     # Reading training set
     train_index = SampleIndex()
 
@@ -435,7 +464,7 @@ def train():
 
     # Building the graph
     optimizer = create_optimizer()
-    gradients, loss = get_tower_results(model_feeder, optimizer, dropout_rates)
+    gradients, loss = get_tower_results(model_feeder, optimizer, dropout_rates, drop_source_layers)
     # Average tower gradients across GPUs
     avg_tower_gradients = average_gradients(gradients)
     log_grads_and_vars(avg_tower_gradients)
@@ -465,20 +494,41 @@ def train():
         log_debug('Session opened.')
         tf.get_default_graph().finalize()
 
-        # Loading or initializing
-        loaded = False
-        if FLAGS.load in ['auto', 'last']:
-            loaded = try_loading(session, checkpoint_saver, checkpoint_filename, 'most recent epoch')
-        if not loaded and FLAGS.load in ['auto', 'best']:
-            loaded = try_loading(session, best_dev_saver, best_dev_filename, 'best validation')
-        if not loaded:
-            if FLAGS.load in ['auto', 'init']:
-                log_info('Initializing...')
-                session.run(initializer)
-            else:
-                log_error('Unable to load %s model from specified checkpoint dir'
-                          ' - consider using load option "auto" or "init".' % FLAGS.load)
-                sys.exit(1)
+        # TRANSFER LEARNING #
+        if FLAGS.source_model_checkpoint_dir:
+            print('Initializing model from', FLAGS.source_model_checkpoint_dir)
+            ckpt = tf.train.load_checkpoint(FLAGS.source_model_checkpoint_dir)
+            variables = list(ckpt.get_variable_to_shape_map().keys())
+
+            # Load desired source variables
+            for v in tf.global_variables():
+                if not any(layer in v.op.name for layer in drop_source_layers):
+                    print('Loading', v.op.name)
+                    v.load(ckpt.get_tensor(v.op.name), session=session)
+                    
+            # Initialize all variables needed for DS, but not loaded from ckpt
+            init_op=tf.variables_initializer(
+                [ v for v in tf.global_variables()
+                  if any(layer in v.op.name
+                         for layer in drop_source_layers)
+                ])
+            session.run(init_op)
+        # TRANSFER LEARNING #
+        
+        # # Loading or initializing
+        # loaded = False
+        # if FLAGS.load in ['auto', 'last']:
+        #     loaded = try_loading(session, checkpoint_saver, checkpoint_filename, 'most recent epoch')
+        # if not loaded and FLAGS.load in ['auto', 'best']:
+        #     loaded = try_loading(session, best_dev_saver, best_dev_filename, 'best validation')
+        # if not loaded:
+        #     if FLAGS.load in ['auto', 'init']:
+        #         log_info('Initializing...')
+        #         session.run(initializer)
+        #     else:
+        #         log_error('Unable to load %s model from specified checkpoint dir'
+        #                   ' - consider using load option "auto" or "init".' % FLAGS.load)
+        #         sys.exit(1)
 
         # Retrieving global_step from restored model and setting training parameters accordingly
         model_feeder.set_data_set(no_dropout_feed_dict, train_set)
