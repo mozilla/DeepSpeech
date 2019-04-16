c34d7db4921b2985aa0e03f6a7c0ddbdbe1c5f7c
josh <jmeyer@mozilla.com>
2019-04-16 20:19:12 +0200

    testing on 1/2/3/4 bytes with LDC run

--- bin/run-ldc93s1.sh
+++ bin/run-ldc93s1.sh
@@ -1,27 +1,20 @@
 #!/bin/sh
-set -xe
-if [ ! -f DeepSpeech.py ]; then
-    echo "Please make sure you run this from DeepSpeech's top level directory."
-    exit 1
-fi;
 
-if [ ! -f "data/ldc93s1/ldc93s1.csv" ]; then
-    echo "Downloading and preprocessing LDC93S1 example data, saving in ./data/ldc93s1."
-    python -u bin/import_ldc93s1.py ./data/ldc93s1
-fi;
+num_bytes=$1
+NEW_TMP=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
-if [ -d "${COMPUTE_KEEP_DIR}" ]; then
-    checkpoint_dir=$COMPUTE_KEEP_DIR
-else
-    checkpoint_dir=$(python -c 'from xdg import BaseDirectory as xdg; print(xdg.save_data_path("deepspeech/ldc93s1"))')
-fi
+
+mkdir /tmp/$NEW_TMP
 
 python -u DeepSpeech.py --noshow_progressbar \
-  --train_files data/ldc93s1/ldc93s1.csv \
-  --test_files data/ldc93s1/ldc93s1.csv \
+  --train_files data/ldc93s1/${num_bytes}-byte/ldc93s1.csv \
+  --test_files data/ldc93s1/${num_bytes}-byte/ldc93s1.csv \
+  --n_layers 1 \
   --train_batch_size 1 \
   --test_batch_size 1 \
   --n_hidden 100 \
-  --epochs 200 \
-  --checkpoint_dir "$checkpoint_dir" \
+  --epochs 1000 \
+  --checkpoint_dir /tmp/$NEW_TMP \
   "$@"
+
+rm -rf /tmp/$NEW_TMP
