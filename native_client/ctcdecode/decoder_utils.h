494b573c80456e26d4311a484c800e0ead1549be
Reuben Morais <reuben.morais@gmail.com>
2019-06-04 11:58:38 -0300

    Address review comments

--- native_client/ctcdecode/decoder_utils.h
+++ native_client/ctcdecode/decoder_utils.h
@@ -62,7 +62,7 @@ std::vector<std::pair<size_t, float>> get_pruned_log_probs(
 // Get beam search result from prefixes in trie tree
 std::vector<Output> get_beam_search_result(
     const std::vector<PathTrie *> &prefixes,
-    size_t beam_size);
+    size_t top_paths);
 
 // Functor for prefix comparsion
 bool prefix_compare(const PathTrie *x, const PathTrie *y);
