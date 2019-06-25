d50c0397c7f11a01ccff1f6825008e14dd8940a5
Reuben Morais <reuben.morais@gmail.com>
2019-06-25 11:43:27 -0300

    Use bazel workspace status to generate versions

---
+++ native_client/gen_workspace_status.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -x
+
+tf_git_version=$(grep "STABLE_TF_GIT_VERSION" "bazel-out/stable-status.txt" | cut -d' ' -f2)
+ds_git_version=$(grep "STABLE_DS_GIT_VERSION" "bazel-out/stable-status.txt" | cut -d' ' -f2)
+ds_graph_version=$(grep "STABLE_DS_GRAPH_VERSION" "bazel-out/stable-status.txt" | cut -d' ' -f2)
+
+cat <<EOF
+const char *tf_local_git_version() {
+    return "${tf_git_version}";
+}
+const char *ds_git_version() {
+    return "${ds_git_version}";
+}
+const int ds_graph_version() {
+    return ${ds_graph_version};
+}
+EOF
---
+++ native_client/workspace_status.h
@@ -0,0 +1,8 @@
+#ifndef WORKSPACE_STATUS_H
+#define WORKSPACE_STATUS_H
+
+const char *tf_local_git_version();
+const char *ds_git_version();
+const int ds_graph_version();
+
+#endif // WORKSPACE_STATUS_H
