0d5ed6e5788d0ede4d155d9cfd63e8a0a806216d
alchemi5t <soundcrane@gmail.com>
2019-07-19 19:47:40 +0530

    pylinting

--- util/feeding.py
+++ util/feeding.py
@@ -32,13 +32,13 @@ def read_csvs(csv_files):
             source_data = source_data.append(file)
     return source_data
 
-def augment(sample,sr):
-    sample = librosa.core.to_mono(np.transpose(sample,(1,0)))
-    if(random.random()<0.5):
-        sample=librosa.effects.pitch_shift(sample,sr,random.randint(0,4)).astype('float32')
+def augment(sample, sr):
+    sample = librosa.core.to_mono(np.transpose(sample, (1, 0)))
+    if random.random() < 0.5:
+        sample = librosa.effects.pitch_shift(sample, sr, random.randint(0, 4)).astype('float32')
     else:
-        sample=librosa.effects.time_stretch(sample,random.uniform(0.8,1.4)).astype('float32')
-    sample=sample.reshape([-1,1])
+        sample = librosa.effects.time_stretch(sample, random.uniform(0.8, 1.4)).astype('float32')
+    sample = sample.reshape([-1, 1])
     return(sample)
 
 def samples_to_mfccs(samples, sample_rate):
@@ -55,8 +55,8 @@ def samples_to_mfccs(samples, sample_rate):
 def audiofile_to_features(wav_filename,augment_data):
     samples = tf.io.read_file(wav_filename)
     decoded = contrib_audio.decode_wav(samples, desired_channels=1)
-    if(augment_data):
-        samples=tf.py_func(augment,[decoded.audio,decoded.sample_rate],tf.float32)
+    if augment_data:
+        samples = tf.py_func(augment, [decoded.audio, decoded.sample_rate], tf.float32)
         features, features_len = samples_to_mfccs(samples, decoded.sample_rate)
     else:
         features, features_len = samples_to_mfccs(decoded.audio, decoded.sample_rate)
