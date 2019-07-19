c25956511dadab06f3c4644cb80b63bf044d26ae
Chirag Ahuja <cahuja1992@gmail.com>
2019-07-19 18:58:42 +0530

    Fixed linter errors

--- util/pertubation.py
+++ util/pertubation.py
@@ -2,12 +2,12 @@ import tensorflow as tf
 import librosa
 import numpy as np
 
-def speed_pertub(samples, fs):
+def speed_pertub(samples):
     samples = samples.flatten().astype('float32')
     speed_range = np.random.uniform(low=0.8, high=1.4)
-    samples = librosa.effects.time_stretch(samples, rate = speed_range)
-    return samples.reshape([-1,1])
+    samples = librosa.effects.time_stretch(samples, rate=speed_range)
+    return samples.reshape([-1, 1])
 
-def pertub(samples, fs):
-    samples = tf.py_func(speed_pertub,[samples, fs], tf.float32)
+def pertub(samples):
+    samples = tf.py_func(speed_pertub,[samples], tf.float32)
     return samples
