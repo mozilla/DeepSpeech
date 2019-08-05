abc05b4a4d104ad0d2be76e5d7ac910ba4a14716
dabinat <dabinat@example.com>
2019-08-05 09:20:13 -0700

    Label validation - Replace hyphens with spaces

--- util/text.py
+++ util/text.py
@@ -92,8 +92,9 @@ def validate_label(label):
     if re.search(r"[0-9]|[(<\[\]&*{]", label) is not None:
         return None
 
-    label = label.replace("-", "")
-    label = label.replace("_", "")
+    label = label.replace("-", " ")
+    label = label.replace("_", " ")
+    label = re.sub("[ ]{2,}", " ", label)
     label = label.replace(".", "")
     label = label.replace(",", "")
     label = label.replace("?", "")
