6dfe8dea54259513aa7f7b9f34d923b8a79b753d
Mahmoud Gamal <Mahmoud.gamal30@yahoo.com>
2019-09-13 10:25:06 +0200

    passes config to list_local_devices

--- util/gpu.py
+++ util/gpu.py
@@ -5,5 +5,5 @@ def get_available_gpus(config):
     r"""
     Returns the number of GPUs available on this system.
     """
-    local_device_protos = device_lib.list_local_devices()
+    local_device_protos = device_lib.list_local_devices(session_config=config)
     return [x.name for x in local_device_protos if x.device_type == 'GPU']
