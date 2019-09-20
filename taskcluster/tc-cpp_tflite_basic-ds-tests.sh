4103247a2136f7c14876cc313e288eaa2a321ff0
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2019-09-20 19:45:19 +0200

    Use TFLite runtime on RPi3/RPi4

--- taskcluster/tc-cpp_tflite_basic-ds-tests.sh
+++ taskcluster/tc-cpp_tflite_basic-ds-tests.sh
@@ -6,7 +6,7 @@ source $(dirname "$0")/tc-tests-utils.sh
 
 model_source=${DEEPSPEECH_TEST_MODEL//.pb/.tflite}
 model_name=$(basename "${model_source}")
-model_name_mmap=$(basename "${model_source}")
+export DEEPSPEECH_ARTIFACTS_ROOT=${DEEPSPEECH_ARTIFACTS_TFLITE_ROOT}
 export DATA_TMP_DIR=${TASKCLUSTER_TMP_DIR}
 
 download_material "${TASKCLUSTER_TMP_DIR}/ds"
@@ -15,8 +15,4 @@ export PATH=${TASKCLUSTER_TMP_DIR}/ds/:$PATH
 
 check_tensorflow_version
 
-run_all_inference_tests
-
-run_multi_inference_tests
-
-run_cpp_only_inference_tests
+run_tflite_basic_inference_tests
