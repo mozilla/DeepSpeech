42e533016cdc2949bd37e3df5bff8d8f6828c185
Carlos Fonseca M <carlfm01@hotmail.com>
2019-11-13 04:25:05 -0600

    Move WPF example to MVVM

--- examples/net_framework/DeepSpeechWPF/App.xaml.cs
+++ examples/net_framework/DeepSpeechWPF/App.xaml.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using CommonServiceLocator;
+using DeepSpeech.WPF.ViewModels;
+using DeepSpeechClient.Interfaces;
+using GalaSoft.MvvmLight.Ioc;
 using System.Windows;
 
 namespace DeepSpeechWPF
@@ -13,5 +11,34 @@ namespace DeepSpeechWPF
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            const int BEAM_WIDTH = 500;
+
+            //Register instance of DeepSpeech
+            DeepSpeechClient.DeepSpeech deepSpeechClient = new DeepSpeechClient.DeepSpeech();
+            try
+            {
+                deepSpeechClient.CreateModel("output_graph.pbmm", BEAM_WIDTH);
+            }
+            catch (System.Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Current.Shutdown();
+            }
+            
+            SimpleIoc.Default.Register<IDeepSpeech>(() => deepSpeechClient);
+            SimpleIoc.Default.Register<MainWindowViewModel>();
+        }
+
+        protected override void OnExit(ExitEventArgs e)
+        {
+            base.OnExit(e);
+            //Dispose instance of DeepSpeech
+            ServiceLocator.Current.GetInstance<IDeepSpeech>()?.Dispose();
+        }
     }
 }
