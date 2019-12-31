ec251367bd012fce533c2ab30f9c8fce8f81ca80
Yi-Hua Chiu <mychiux413@gmail.com>
2019-12-31 11:20:58 +0800

    mix noise works, but performance is bad

--- util/decoded_augmentation.py
+++ util/decoded_augmentation.py
@@ -8,9 +8,9 @@ def augment_noise(audio,
                   walk_dirs,
                   change_audio_db_max=0,
                   change_audio_db_min=-10,
-                  change_noise_db_max=-25,
-                  change_noise_db_min=-50
-                  ):
+                  change_noise_db_max=-15,
+                  change_noise_db_min=-25):
+    assert isinstance(walk_dirs, list)
     noise_filenames = []
     for d in walk_dirs:
         for dirpath, _, filenames in os.walk(d):
@@ -31,10 +31,10 @@ def augment_noise(audio,
     noise_decoded_audio_len = tf.shape(noise_audio)[0]
 
     multiply = tf.math.floordiv(decoded_audio_len, noise_decoded_audio_len) + 1
-    noise_audio = tf.tile(noise_audio, [multiply, 1])
+    noise_audio_tile = tf.tile(noise_audio, [multiply, 1])
 
     # now noise_decoded_len must > decoded_len
-    noise_decoded_audio_len = tf.shape(noise_audio)[0]
+    noise_decoded_audio_len = tf.shape(noise_audio_tile)[0]
 
     mix_decoded_start_end_points = tfv1.random_uniform(
         [2], minval=0, maxval=decoded_audio_len-1, dtype=tf.int32)
@@ -48,7 +48,7 @@ def augment_noise(audio,
     mix_noise_decoded_start_point = tfv1.random_uniform(
         [], minval=0, maxval=noise_decoded_audio_len - mix_decoded_width, dtype=tf.int32)
     mix_noise_decoded_end_point = mix_noise_decoded_start_point + mix_decoded_width
-    extract_noise_decoded = noise_audio[mix_noise_decoded_start_point:mix_noise_decoded_end_point, :]
+    extract_noise_decoded = noise_audio_tile[mix_noise_decoded_start_point:mix_noise_decoded_end_point, :]
 
     right_zeros = tf.zeros(
         shape=[decoded_audio_len - mix_decoded_end_point, 1])
@@ -58,10 +58,11 @@ def augment_noise(audio,
 
     choosen_audio_db = tfv1.random_uniform(
         [], minval=change_audio_db_min, maxval=change_audio_db_max)
-    audio_ratio = tf.math.exp(choosen_audio_db / 10)
+    audio_ratio = tf.math.pow(10.0, choosen_audio_db / 10)
 
     choosen_noise_db = tfv1.random_uniform(
         [], minval=change_noise_db_min, maxval=change_noise_db_max)
-    noise_ratio = tf.math.exp(choosen_noise_db / 10)
-
+    # choosen_noise_db = tf.random.normal(
+    #     [], mean=change_noise_db_max, stddev=change_noise_db_min)
+    noise_ratio = tf.math.pow(10.0, choosen_noise_db / 10)
     return tf.multiply(audio, audio_ratio) + tf.multiply(mixed_noise, noise_ratio)
