29a92e098fcc8d71d1501727daa79ef85dfecb04
juandspy <42124482+juandspy@users.noreply.github.com>
2020-01-22 16:22:15 +0100

    Update taskcluster.py
    I copied ``maybe_download_tc_bin`` syntax in order to make the code easier to follow.

--- util/taskcluster.py
+++ util/taskcluster.py
@@ -147,7 +147,9 @@ def main():
     maybe_download_tc(target_dir=args.target, tc_url=get_tc_url(args.arch, args.artifact, args.branch))
 
     if args.artifact == "convert_graphdef_memmapped_format":
-        subprocess.check_call(['chmod', '+x', os.path.join(args.target, args.artifact)])
+        convert_graph_file = os.path.join(args.target, args.artifact)
+        final_stat = os.stat(convert_graph_file)
+        os.chmod(convert_graph_file, final_stat.st_mode | stat.S_IEXEC)
     
     if '.tar.' in args.artifact:
         subprocess.check_call(['tar', 'xvf', os.path.join(args.target, args.artifact), '-C', args.target])
