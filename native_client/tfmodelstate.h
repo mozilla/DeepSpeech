656eea4622c541cc7f3cb70d43c6916ae054f750
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2020-01-28 09:54:34 +0100

    Use std::unique_ptr<> for TensorFlow session

--- native_client/tfmodelstate.h
+++ native_client/tfmodelstate.h
@@ -11,8 +11,8 @@
 
 struct TFModelState : public ModelState
 {
-  tensorflow::MemmappedEnv* mmap_env_;
-  tensorflow::Session* session_;
+  std::unique_ptr<tensorflow::MemmappedEnv> mmap_env_;
+  std::unique_ptr<tensorflow::Session> session_;
   tensorflow::GraphDef graph_def_;
 
   TFModelState();
