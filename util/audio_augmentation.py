9e2648a47c04010e8ba28121fab9e0dd8f2bd010
Yi-Hua Chiu <mychiux413@gmail.com>
2020-02-22 00:40:09 +0800

    [MOD] mix noise into complete audio

--- util/audio_augmentation.py
+++ util/audio_augmentation.py
@@ -25,33 +25,18 @@ def augment_noise(audio,
                   change_noise_db_min=-25):
 
     decoded_audio_len = tf.shape(audio)[0]
-    noise_decoded_audio_len = tf.shape(noise_audio)[0]
+    decoded_noise_len = tf.shape(noise_audio)[0]
 
-    multiply = tf.math.floordiv(decoded_audio_len, noise_decoded_audio_len) + 1
+    multiply = tf.math.floordiv(decoded_audio_len, decoded_noise_len) + 1
     noise_audio_tile = tf.tile(noise_audio, [multiply, 1])
 
-    # now noise_decoded_len must > decoded_len
-    noise_decoded_audio_len = tf.shape(noise_audio_tile)[0]
+    # Now, decoded_noise_len must > decoded_audio_len
+    decoded_noise_len = tf.shape(noise_audio_tile)[0]
 
-    mix_decoded_start_end_points = tfv1.random_uniform(
-        [2], minval=0, maxval=decoded_audio_len-1, dtype=tf.int32)
-    mix_decoded_start_point = tf.math.reduce_min(mix_decoded_start_end_points)
-    mix_decoded_end_point = tf.math.reduce_max(
-        mix_decoded_start_end_points) + 1
-    mix_decoded_width = mix_decoded_end_point - mix_decoded_start_point
-
-    left_zeros = tf.zeros(shape=[mix_decoded_start_point, 1])
-
-    mix_noise_decoded_start_point = tfv1.random_uniform(
-        [], minval=0, maxval=noise_decoded_audio_len - mix_decoded_width, dtype=tf.int32)
-    mix_noise_decoded_end_point = mix_noise_decoded_start_point + mix_decoded_width
-    extract_noise_decoded = noise_audio_tile[mix_noise_decoded_start_point:mix_noise_decoded_end_point, :]
-
-    right_zeros = tf.zeros(
-        shape=[decoded_audio_len - mix_decoded_end_point, 1])
-
-    mixed_noise = tf.concat(
-        [left_zeros, extract_noise_decoded, right_zeros], axis=0)
+    mix_decoded_start_point = tfv1.random_uniform(
+        [], minval=0, maxval=decoded_noise_len-decoded_audio_len, dtype=tf.int32)
+    mix_decoded_end_point = mix_decoded_start_point + decoded_audio_len
+    extract_noise_decoded = noise_audio_tile[mix_decoded_start_point:mix_decoded_end_point, :]
 
     choosen_audio_db = tfv1.random_uniform(
         [], minval=change_audio_db_min, maxval=change_audio_db_max)
@@ -60,5 +45,5 @@ def augment_noise(audio,
     choosen_noise_db = tfv1.random_uniform(
         [], minval=change_noise_db_min, maxval=change_noise_db_max)
     noise_ratio = tf.math.pow(10.0, choosen_noise_db / 10)
-    mixed_audio = tf.multiply(audio, audio_ratio) + tf.multiply(mixed_noise, noise_ratio)
+    mixed_audio = tf.multiply(audio, audio_ratio) + tf.multiply(extract_noise_decoded, noise_ratio)
     return tf.maximum(tf.minimum(mixed_audio, 1.0), -1.0)
