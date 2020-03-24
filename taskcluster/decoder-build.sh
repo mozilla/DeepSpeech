bbf7cf82ba611c3d65b4856b1214bc6ce21e3606
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2020-03-24 12:51:46 +0100

    Add CTC decoder build on TaskCluster

--- taskcluster/decoder-build.sh
+++ taskcluster/decoder-build.sh
@@ -6,6 +6,10 @@ source $(dirname "$0")/tc-tests-utils.sh
 
 source ${DS_ROOT_TASK}/DeepSpeech/tf/tc-vars.sh
 
-export SYSTEM_TARGET=host
+if [ "${OS}" = "${TC_MSYS_VERSION}" ]; then
+  export SYSTEM_TARGET=host-win
+else
+  export SYSTEM_TARGET=host
+fi;
 
 do_deepspeech_decoder_build
