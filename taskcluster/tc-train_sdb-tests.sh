ebe2fc0470bb43c461e904c27b44ae07c7fa0aba
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2020-05-02 16:21:18 +0200

    Split TaskCluster training

--- taskcluster/tc-train_sdb-tests.sh
+++ taskcluster/tc-train_sdb-tests.sh
@@ -8,13 +8,6 @@ extract_python_versions "$1" "pyver" "pyver_pkg" "py_unicode_type" "pyconf" "pya
 
 bitrate=$2
 
-decoder_src=$3
-
-if [ "$decoder_src" = "--pypi" ]; then
-    # Disable automatically picking up decoder package built in this CI group
-    export DECODER_ARTIFACTS_ROOT=""
-fi
-
 mkdir -p ${TASKCLUSTER_ARTIFACTS} || true
 mkdir -p /tmp/train || true
 mkdir -p /tmp/train_tflite || true
@@ -46,20 +39,13 @@ echo "Moving ${sample_name} to LDC93S1.wav"
 mv "${DS_ROOT_TASK}/DeepSpeech/ds/data/smoke_test/${sample_name}" "${DS_ROOT_TASK}/DeepSpeech/ds/data/smoke_test/LDC93S1.wav"
 
 pushd ${HOME}/DeepSpeech/ds/
-    # Run twice to test preprocessed features
-    time ./bin/run-tc-ldc93s1_new.sh 249 "${sample_rate}"
-    time ./bin/run-tc-ldc93s1_new.sh 1 "${sample_rate}"
-    time ./bin/run-tc-ldc93s1_tflite.sh "${sample_rate}"
+    # Testing single SDB source
+    time ./bin/run-tc-ldc93s1_new_sdb.sh 220 "${sample_rate}"
+    # Testing interleaved source (SDB+CSV combination) - run twice to test preprocessed features
+    time ./bin/run-tc-ldc93s1_new_sdb_csv.sh 109 "${sample_rate}"
+    time ./bin/run-tc-ldc93s1_new_sdb_csv.sh 1 "${sample_rate}"
+    time ./bin/run-tc-ldc93s1_checkpoint.sh
+    time ./bin/run-tc-ldc93s1_checkpoint_sdb.sh
 popd
-
-cp /tmp/train/output_graph.pb ${TASKCLUSTER_ARTIFACTS}
-cp /tmp/train_tflite/output_graph.tflite ${TASKCLUSTER_ARTIFACTS}
-
-pushd ${HOME}/DeepSpeech/ds/
-    python util/taskcluster.py --source tensorflow --artifact convert_graphdef_memmapped_format --branch r1.15 --target /tmp/
-popd
-
-/tmp/convert_graphdef_memmapped_format --in_graph=/tmp/train/output_graph.pb --out_graph=/tmp/train/output_graph.pbmm
-cp /tmp/train/output_graph.pbmm ${TASKCLUSTER_ARTIFACTS}
 
 virtualenv_deactivate "${pyalias}" "deepspeech"
