ccca1c1fed17e5586cce43a6519d0914761c1e2c
Shubham Kumar <Shubhamkrai123@gmail.com>
2020-05-29 20:28:52 +0530

    add tests to TC and update travis

--- taskcluster/tc-train-unittests.sh
+++ taskcluster/tc-train-unittests.sh
@@ -4,13 +4,7 @@ set -xe
 
 source $(dirname "$0")/tc-tests-utils.sh
 
-extract_python_versions "$1" "pyver" "pyver_pkg" "py_unicode_type" "pyconf" "pyalias"
-
-bitrate=$2
-
 mkdir -p ${TASKCLUSTER_ARTIFACTS} || true
-mkdir -p /tmp/train || true
-mkdir -p /tmp/train_tflite || true
 
 virtualenv_activate "${pyalias}" "deepspeech"
 
@@ -18,7 +12,6 @@ set -o pipefail
 pip install --upgrade pip==19.3.1 setuptools==45.0.0 wheel==0.33.6 | cat
 pushd ${HOME}/DeepSpeech/ds
     pip install --upgrade . | cat
+    python -m unittest
 popd
 set +o pipefail
-
-python -m unittest
