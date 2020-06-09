c85f95f781ee8c007502bcfc4ab2e24fc3e3e24c
Reuben Morais <reuben.morais@gmail.com>
2020-06-09 19:58:04 +0200

    Add DeepSpeech iOS tasks

---
+++ taskcluster/ios-build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -xe
+
+source $(dirname "$0")/tc-tests-utils.sh
+
+source ${DS_ROOT_TASK}/DeepSpeech/tf/tc-vars.sh
+
+BAZEL_TARGETS="
+//native_client:libdeepspeech.so
+"
+
+BAZEL_BUILD_FLAGS="--config=ios_arm64 --define=runtime=tflite ${BAZEL_OPT_FLAGS} ${BAZEL_EXTRA_FLAGS}"
+
+BAZEL_ENV_FLAGS="TF_NEED_CUDA=0"
+
+do_bazel_build
