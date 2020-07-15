2f568e77858c8bea93c67362b0f16c458bf481f7
Reuben Morais <reuben.morais@gmail.com>
2020-07-15 14:38:56 +0200

    Don't use BAZEL_OPT_FLAGS in iOS builds

--- taskcluster/ios-build.sh
+++ taskcluster/ios-build.sh
@@ -10,7 +10,7 @@ BAZEL_TARGETS="
 //native_client:libdeepspeech.so
 "
 
-BAZEL_BUILD_FLAGS="--config=ios_arm64 --define=runtime=tflite ${BAZEL_OPT_FLAGS} ${BAZEL_EXTRA_FLAGS}"
+BAZEL_BUILD_FLAGS="--config=ios_arm64 --define=runtime=tflite ${BAZEL_EXTRA_FLAGS}"
 
 BAZEL_ENV_FLAGS="TF_NEED_CUDA=0"
 
