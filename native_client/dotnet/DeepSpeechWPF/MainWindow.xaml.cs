c55143d282388a6719d7817580b462705755116c
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2020-07-31 16:54:46 +0200

    Fix #3198: Do not rely on examples repo for building .Net

---
+++ native_client/dotnet/DeepSpeechWPF/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using CommonServiceLocator;
+using DeepSpeech.WPF.ViewModels;
+using System.Windows;
+
+namespace DeepSpeechWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow() => InitializeComponent();
+
+        private void Window_Loaded(object sender, RoutedEventArgs e) =>
+            DataContext = ServiceLocator.Current.GetInstance<MainWindowViewModel>();
+    }
+}
