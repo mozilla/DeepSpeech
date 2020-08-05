4d98958b778ae4010ce938fa79f700d6a75a2722
Reuben Morais <reuben.morais@gmail.com>
2020-08-05 14:44:34 +0200

    iOS: Re-share workspace schemes and fix packaging

--- taskcluster/ios-package.sh
+++ taskcluster/ios-package.sh
@@ -17,14 +17,14 @@ package_libdeepspeech_as_zip "libmozilla_voice_stt.zip"
 case $arch in
 "--x86_64")
   release_folder="Release-iphonesimulator"
-  artifact_name="deepspeech_ios.framework.x86_64.tar.xz"
+  artifact_name="mozilla_voice_stt.framework.x86_64.tar.xz"
   ;;
 "--arm64")
   release_folder="Release-iphoneos"
-  artifact_name="deepspeech_ios.framework.arm64.tar.xz"
+  artifact_name="mozilla_voice_stt.framework.arm64.tar.xz"
 ;;
 esac
 
 ${TAR} -cf - \
-       -C ${DS_ROOT_TASK}/DeepSpeech/ds/native_client/swift/DerivedData/Build/Products/${release_folder}/ deepspeech_ios.framework \
+       -C ${DS_ROOT_TASK}/DeepSpeech/ds/native_client/swift/DerivedData/Build/Products/${release_folder}/ mozilla_voice_stt.framework \
        | ${XZ} > "${TASKCLUSTER_ARTIFACTS}/${artifact_name}"
