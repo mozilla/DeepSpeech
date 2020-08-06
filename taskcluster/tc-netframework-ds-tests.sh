1e8213c38558b4b7e0a9c1471419138d3693a332
Reuben Morais <reuben.morais@gmail.com>
2020-08-06 16:16:33 +0200

    Rename NuGet -GPU package to -CUDA

--- taskcluster/tc-netframework-ds-tests.sh
+++ taskcluster/tc-netframework-ds-tests.sh
@@ -11,7 +11,7 @@ bitrate=$1
 set_ldc_sample_filename "${bitrate}"
 
 if [ "${package_option}" = "cuda" ]; then
-    PROJECT_NAME="Mozilla-Voice-STT-GPU"
+    PROJECT_NAME="Mozilla-Voice-STT-CUDA"
 elif [ "${package_option}" = "--tflite" ]; then
     PROJECT_NAME="Mozilla-Voice-STT-TFLite"
     model_source=${DEEPSPEECH_TEST_MODEL//.pb/.tflite}
