7f75e13f5ec8430ec9907bf6206791d61373b41b
Bernardo Henz <bernardohenz@gmail.com>
2020-08-18 11:26:39 -0300

    Implementation of layer-norm in the training script

--- training/mozilla_voice_stt_training/util/flags.py
+++ training/mozilla_voice_stt_training/util/flags.py
@@ -135,6 +135,7 @@ def create_flags():
     # Geometry
 
     f.DEFINE_integer('n_hidden', 2048, 'layer width to use when initialising layers')
+    f.DEFINE_boolean('layer_norm', True, 'wether to use layer-normalization after each fully-connected layer (except the last one)')
 
     # Initialization
 
