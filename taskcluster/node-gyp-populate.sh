a4d6c672d41f61f479fc7c72a31be54bcee4c592
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2020-09-10 08:50:59 +0200

    Fix #3316: Add Electron 10.x

--- taskcluster/node-gyp-populate.sh
+++ taskcluster/node-gyp-populate.sh
@@ -13,23 +13,27 @@ npm install node-gyp@6.x
 export PATH=$HOME/node_modules/.bin/:$PATH
 
 devDir=$DS_ROOT_TASK/node-gyp-cache/
+nodejsDevDir=${devDir}/nodejs/
+electronjsDevDir=${devDir}/electronjs/
 
-mkdir -p ${devDir}
-
-node-gyp list ${devDir}
+mkdir -p ${nodejsDevDir} ${electronjsDevDir}
+node-gyp list ${nodejsDevDir} ${electronjsDevDir}
 
 for node in ${SUPPORTED_NODEJS_BUILD_VERSIONS}; do
-    node-gyp install --devdir ${devDir} \
+    node-gyp install --devdir ${nodejsDevDir} \
         --target=$node
-    mkdir ${devDir}/${node}/x64/ || true
-    curl -sSL https://nodejs.org/dist/v${node}/win-x64/node.lib -o ${devDir}/${node}/x64/node.lib
+    mkdir ${nodejsDevDir}/${node}/x64/ || true
+    curl -sSL https://nodejs.org/dist/v${node}/win-x64/node.lib -o ${nodejsDevDir}/${node}/x64/node.lib
 done;
 
+mkdir -p ${electronjsDevDir}
+node-gyp list ${electronjsDevDir}
+
 for electron in ${SUPPORTED_ELECTRONJS_VERSIONS}; do
-    node-gyp install --devdir  ${devDir} \
+    node-gyp install --devdir  ${electronjsDevDir} \
         --target=$electron \
         --disturl=https://electronjs.org/headers \
         --runtime=electron
-    mkdir ${devDir}/${electron}/x64/ || true
-    curl -sSL https://electronjs.org/headers/v${electron}/win-x64/node.lib -o ${devDir}/${electron}/x64/node.lib
+    mkdir ${electronjsDevDir}/${electron}/x64/ || true
+    curl -sSL https://electronjs.org/headers/v${electron}/win-x64/node.lib -o ${electronjsDevDir}/${electron}/x64/node.lib
 done;
