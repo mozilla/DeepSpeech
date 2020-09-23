4945380e8b5f2487fc68029b9bcfea04dd4b4bb9
josh meyer <joshua.richard.meyer@gmail.com>
2020-09-23 05:19:53 -0700

    add DS_BINARY_PREFIX to tc-asserts.sh for hotwords command

--- taskcluster/tc-asserts.sh
+++ taskcluster/tc-asserts.sh
@@ -527,7 +527,7 @@ run_multi_inference_tests()
 run_hotword_tests()
 {
   set +e
-  hotwords_decode=$(deepspeech --model ${TASKCLUSTER_TMP_DIR}/${model_name_mmap} --scorer ${TASKCLUSTER_TMP_DIR}/kenlm.scorer --audio ${TASKCLUSTER_TMP_DIR}/${ldc93s1_sample_filename} --hot_words "foo:0.0,bar:-0.1" 2>${TASKCLUSTER_TMP_DIR}/stderr)
+  hotwords_decode=$(${DS_BINARY_PREFIX}deepspeech --model ${TASKCLUSTER_TMP_DIR}/${model_name_mmap} --scorer ${TASKCLUSTER_TMP_DIR}/kenlm.scorer --audio ${TASKCLUSTER_TMP_DIR}/${ldc93s1_sample_filename} --hot_words "foo:0.0,bar:-0.1" 2>${TASKCLUSTER_TMP_DIR}/stderr)
   status=$?
   set -e
   assert_correct_ldc93s1_lm "${hotwords_decode}" "$status"
