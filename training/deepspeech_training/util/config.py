42170a57eb4d14120b847cde95998b3c91d9b7d7
CatalinVoss <catalin@cs.stanford.edu>
2020-11-12 10:46:49 -0800

    Remote I/O for config

--- training/deepspeech_training/util/config.py
+++ training/deepspeech_training/util/config.py
@@ -13,7 +13,7 @@ from .gpu import get_available_gpus
 from .logging import log_error, log_warn
 from .helpers import parse_file_size
 from .augmentations import parse_augmentations
-
+from .util.io import path_exists_remote
 
 class ConfigSingleton:
     _config = None
@@ -139,7 +139,7 @@ def initialize_globals():
     c.audio_step_samples = FLAGS.audio_sample_rate * (FLAGS.feature_win_step / 1000)
 
     if FLAGS.one_shot_infer:
-        if not os.path.exists(FLAGS.one_shot_infer):
+        if not path_exists_remote(FLAGS.one_shot_infer):
             log_error('Path specified in --one_shot_infer is not a valid file.')
             sys.exit(1)
 
