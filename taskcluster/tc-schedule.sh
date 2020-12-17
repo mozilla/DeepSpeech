bb1ad0019497492f925c43721a4ccef373b7be66
Reuben Morais <reuben.morais@gmail.com>
2020-12-17 19:03:44 +0000

    Convert to .taskcluster.yml v1
    forward TASK_ID, add created and deadline
    
    more fixes
    
    typo
    
    try without TASK_ID
    
    fix task templates
    
    add missing env vars to tc decision dry runs
    
    avoid repetition in .taskcluster and manually forward varibles to tc-decision.py
    
    url -> clone_url
    
    simulate GITHUB_EVENT
    
    separate ref an sha
    
    correct pull request actions
    
    correct pull request policy

--- taskcluster/tc-schedule.sh
+++ taskcluster/tc-schedule.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
-
 set -ex
+
+# Set environment variables passed in arguments
+for line in $@; do
+  eval "$line"
+done
 
 curdir=$(dirname "$0")/
 
@@ -14,14 +18,41 @@ pip3 install --quiet --user --upgrade -r ${curdir}/tc-decision_reqs.txt
 # This should help us track merge failures in advance
 for event in pull_request.opened pull_request.synchronize pull_request.reopened push;
 do
-    GITHUB_EVENT="${event}" TASK_ID="aa" GITHUB_HEAD_BRANCHORTAG="branchName" GITHUB_HEAD_REF="refs/heads/branchName" python3 ${curdir}/tc-decision.py --dry
+    GITHUB_EVENT="${event}" \
+    TASK_ID="aa" \
+    GITHUB_HEAD_BRANCHORTAG="branchName" \
+    GITHUB_HEAD_REF="refs/heads/branchName" \
+    GITHUB_HEAD_SHA="branchName" \
+    GITHUB_HEAD_REPO_URL="url" \
+    GITHUB_HEAD_USER_LOGIN="user" \
+    GITHUB_HEAD_USER_EMAIL="email" \
+    python3 ${curdir}/tc-decision.py --dry
 done;
 
-GITHUB_EVENT="tag" TASK_ID="aa" GITHUB_HEAD_BRANCHORTAG="tagName" GITHUB_HEAD_REF="refs/tags/tagName" python3 ${curdir}/tc-decision.py --dry
+GITHUB_EVENT="tag" \
+TASK_ID="aa" \
+GITHUB_HEAD_BRANCHORTAG="tagName" \
+GITHUB_HEAD_REF="refs/tags/tagName" \
+GITHUB_HEAD_SHA="branchName" \
+GITHUB_HEAD_REPO_URL="url" \
+GITHUB_HEAD_USER_LOGIN="user" \
+GITHUB_HEAD_USER_EMAIL="email" \
+python3 ${curdir}/tc-decision.py --dry
 
 # Create a new env variable for usage in TaskCluster .yml files
-export GITHUB_HEAD_BRANCHORTAG="${GITHUB_HEAD_BRANCH}${GITHUB_HEAD_TAG}"
+GITHUB_HEAD_BRANCHORTAG="${GITHUB_HEAD_BRANCH}${GITHUB_HEAD_TAG}"
 
 # Quick hack because tc-decision uses GITHUB_HEAD_BRANCH
-export GITHUB_HEAD_BRANCH="${GITHUB_HEAD_BRANCH}${GITHUB_HEAD_TAG}"
+GITHUB_HEAD_BRANCH="${GITHUB_HEAD_BRANCH}${GITHUB_HEAD_TAG}"
+
+# Forward all variables to tc-decision.py
+GITHUB_EVENT="$GITHUB_EVENT" \
+TASK_ID="$TASK_ID" \
+GITHUB_HEAD_BRANCHORTAG="${GITHUB_HEAD_BRANCHORTAAG}" \
+GITHUB_HEAD_BRANCH="${GITHUB_HEAD_BRANCH}" \
+GITHUB_HEAD_REF="$GITHUB_HEAD_REF" \
+GITHUB_HEAD_SHA="$GITHUB_HEAD_SHA" \
+GITHUB_HEAD_REPO_URL="$GITHUB_HEAD_REPO_URL" \
+GITHUB_HEAD_USER_LOGIN="$GITHUB_HEAD_USER_LOGIN" \
+GITHUB_HEAD_USER_EMAIL="$GITHUB_HEAD_USER_EMAIL" \
 python3 ${curdir}/tc-decision.py
