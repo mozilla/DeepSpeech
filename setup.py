85b9f0fd3d831d9789bfa8038510b0433095c6fb
Reuben Morais <reuben.morais@gmail.com>
2021-01-02 13:28:04 +0000

    Bump training dependencies to TensorFlow 2.3.1

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ def main():
     ]
 
     tensorflow_pypi_dep = [
-        'tensorflow == 1.15.4'
+        'tensorflow == 2.3.1'
     ]
 
     # Due to pip craziness environment variables are the only consistent way to
