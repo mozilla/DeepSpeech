fee12be4ffe3b77a4adb525807a3275344f73830
Alexandre Lissy <lissyx@lissyx.dyndns.org>
2021-02-25 10:07:58 +0100

    Update SWIG with upstream 4.1-aligned branch

--- native_client/definitions.mk
+++ native_client/definitions.mk
@@ -209,11 +209,11 @@ endef
 SWIG_DIST_URL ?=
 ifeq ($(SWIG_DIST_URL),)
 ifeq ($(findstring Linux,$(OS)),Linux)
-SWIG_DIST_URL := "https://community-tc.services.mozilla.com/api/index/v1/task/project.deepspeech.swig.linux.amd64.f0e5d1a0be7383abd98a29a75f47d5dc10a87ef2.0/artifacts/public/ds-swig.tar.gz"
+SWIG_DIST_URL := "https://community-tc.services.mozilla.com/api/index/v1/task/project.deepspeech.swig.linux.amd64.fec7d5d3179833e37759ffc6532f86344982e26a.0/artifacts/public/ds-swig.tar.gz"
 else ifeq ($(findstring Darwin,$(OS)),Darwin)
-SWIG_DIST_URL := "https://community-tc.services.mozilla.com/api/index/v1/task/project.deepspeech.swig.darwin.amd64.f0e5d1a0be7383abd98a29a75f47d5dc10a87ef2.0/artifacts/public/ds-swig.tar.gz"
+SWIG_DIST_URL := "https://community-tc.services.mozilla.com/api/index/v1/task/project.deepspeech.swig.darwin.amd64.fec7d5d3179833e37759ffc6532f86344982e26a.0/artifacts/public/ds-swig.tar.gz"
 else ifeq ($(findstring _NT,$(OS)),_NT)
-SWIG_DIST_URL := "https://community-tc.services.mozilla.com/api/index/v1/task/project.deepspeech.swig.win.amd64.f0e5d1a0be7383abd98a29a75f47d5dc10a87ef2.0/artifacts/public/ds-swig.tar.gz"
+SWIG_DIST_URL := "https://community-tc.services.mozilla.com/api/index/v1/task/project.deepspeech.swig.win.amd64.fec7d5d3179833e37759ffc6532f86344982e26a.0/artifacts/public/ds-swig.tar.gz"
 else
 $(error There is no prebuilt SWIG available for your platform. Please produce one and set SWIG_DIST_URL.)
 endif # findstring()
@@ -224,7 +224,7 @@ SWIG_ROOT ?= $(abspath $(shell dirname "$(lastword $(MAKEFILE_LIST))"))/ds-swig
 ifeq ($(findstring _NT,$(OS)),_NT)
 SWIG_ROOT ?= $(shell cygpath -u "$(SWIG_ROOT)")
 endif
-SWIG_LIB ?= $(SWIG_ROOT)/share/swig/4.0.2/
+SWIG_LIB ?= $(SWIG_ROOT)/share/swig/4.1.0/
 
 SWIG_BIN := swig$(PLATFORM_EXE_SUFFIX)
 DS_SWIG_BIN := ds-swig$(PLATFORM_EXE_SUFFIX)
