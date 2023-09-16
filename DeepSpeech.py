24c99f554fa61f5ec4a97fd53910a2ff5d0ccbf6
Krunal6767 <106827625+Krunal6767@users.noreply.github.com>
2023-09-16 01:36:12 -0400

    Update DeepSpeech.py

--- DeepSpeech.py
+++ DeepSpeech.py
@@ -2,11 +2,29 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import, division, print_function
 
-if __name__ == '__main__':
+import sys
+import subprocess
+
+def check_training_package():
+    try:
+        import deepspeech_training  # Check if the training package is installed
+        return True
+    except ImportError:
+        return False
+
+def run_training():
     try:
         from deepspeech_training import train as ds_train
+        ds_train.run_script()  # Run the DeepSpeech training script
     except ImportError:
         print('Training package is not installed. See training documentation.')
-        raise
+    except Exception as e:
+        print(f'An error occurred during training: {e}')
 
-    ds_train.run_script()
+if __name__ == '__main__':
+    if not check_training_package():
+        print('DeepSpeech training package is not installed.')
+        print('Please install it by following the training documentation.')
+        sys.exit(1)
+
+    run_training()
