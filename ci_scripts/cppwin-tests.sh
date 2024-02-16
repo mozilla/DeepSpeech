19127c6747e2e05518b2c9b1451c7b09fa102e45
Shrutakeerti_1111 <121077624+Shrutakeerti@users.noreply.github.com>
2024-02-16 15:40:03 +0530

    Update cppwin-tests.sh

--- ci_scripts/cppwin-tests.sh
+++ ci_scripts/cppwin-tests.sh
@@ -2,19 +2,20 @@
 
 set -xe
 
-source $(dirname "$0")/all-vars.sh
-source $(dirname "$0")/all-utils.sh
-source $(dirname "$0")/asserts.sh
+source "$(dirname "$0")/all-vars.sh"
+source "$(dirname "$0")/all-utils.sh"
+source "$(dirname "$0")/asserts.sh"
 
 bitrate=$1
 set_ldc_sample_filename "${bitrate}"
 
 download_material "${CI_TMP_DIR}/ds"
 
-export PATH=${CI_TMP_DIR}/ds/:$PATH
+export PATH="${CI_TMP_DIR}/ds/:$PATH"
 
 check_versions
 
 ensure_cuda_usage "$2"
 
 run_basic_inference_tests
+
